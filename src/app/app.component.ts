e8c1a55fca7ec132b97a41def5accf87abe8d319
Anastasia <anastaisafedotova@gmail.com>
2021-06-24 01:11:43 +0200

    refactor: start include socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+import io from 'socket.io-client';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor() {
+    const socket = io(environment.url);
+
+    socket.on("message", data => {
+      console.log(data);
+    });
+  }
 }
